4795b2d14efe090240c7730a4dc95a3ea9cb3072
root <root@GGbao>
2025-11-18 16:13:09 +0800

    feat: 新功能2

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MyPlugin(Star):
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
+        logger.info("测试--------------------------------------------------------------------");
         """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         self.counter += 1  # 每次调用计数器+1
         # user_name = event.get_sender_name()  这个第一次部署时候 拿不到这个 user_name
